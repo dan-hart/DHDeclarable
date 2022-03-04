237e39a2c7e9289fb7e9715a9f2a38cd8fa6a52d
Dan Hart <dhart94@gmail.com>
2022-03-04 13:29:10 -0500

    UIView+rounded

---
+++ Tests/DHDeclarableTests/ExtensionTests/UIKitTests/UIViewTests/UIView+roundedTests.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+roundedTests.swift
+//  
+//
+//  Created by Dan Hart on 3/4/22.
+//
+
+@testable import DHDeclarable
+import UIKit
+import XCTest
+
+class UIView_roundedTests: XCTestCase {
+    var testCornerRadiusValue: CGFloat = 8
+    
+    func testCornerRadius() {
+        let view = UIView()
+        view.cornerRadius = testCornerRadiusValue
+        XCTAssertEqual(view.layer.cornerRadius, testCornerRadiusValue)
+        XCTAssertTrue(view.layer.masksToBounds)
+    }
+    
+    func testRounded() {
+        let view = UIView()
+        XCTAssertEqual(view.layer.cornerRadius, 0)
+        XCTAssertNotNil(view.rounded(by: testCornerRadiusValue))
+        XCTAssertEqual(view.cornerRadius, testCornerRadiusValue)
+    }
+}

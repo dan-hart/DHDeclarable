d1376373fb6d254598d2995f16d43b814085a9f3
Dan Hart <dhart94@gmail.com>
2022-03-09 13:16:20 -0500

    Add documentation

--- Sources/DHDeclarable/DHDeclarable.swift
+++ Sources/DHDeclarable/DHDeclarable.swift
@@ -8,33 +8,48 @@
 import Foundation
 
 // MARK: - ObjectDHDeclarable
+/// The foundational object for all other objects in `DHDeclarable`
 public protocol ObjectDHDeclarable: AnyObject {
+    /// Generic for use with any type
     associatedtype T
 
     /// Provides a then to configure instances inline.
     /// - Parameter then: A then `self` as the argument.
-    /// - Returns: Simply returns the instance after called the `then`.
+    /// - Returns: the instance after called the `then`.
     @discardableResult func declaredWith(_ then: (_ instance: T) -> Void) -> T
 
+    /// Provides a then to configure instance inline based on a boolean test
+    /// - Parameter then: A then `self` as the argument if the boolean test is true
+    /// - Returns: the instance after called the `then`
     @discardableResult func declaredIf(_ value: Bool, _ then: (_ instance: T) -> Void) -> T
 
     /// Provides a then to configure instance inline based on a boolean test
     /// - Parameter then: A then `self` as the argument if the boolean test is true
     /// - Parameter otherwise: A then `self` as the argument if the boolean test is false
-    /// - Returns: Simply returns the instance after called the `then` or `otherwise`
+    /// - Returns: the instance after called the `then` or `otherwise`
     @discardableResult func declaredIf(_ value: Bool, _ then: (_ instance: T) -> Void, otherwise: ((_ instance: T) -> Void)?) -> T
 }
 
 public extension ObjectDHDeclarable {
+    /// Provides a then to configure instances inline.
+    /// - Parameter then: A then `self` as the argument.
+    /// - Returns: Simply returns the instance after called the `then`.
     @discardableResult func declaredWith(_ then: (_ instance: Self) -> Void) -> Self {
         then(self)
         return self
     }
 
+    /// Provides a then to configure instance inline based on a boolean test
+    /// - Parameter then: A then `self` as the argument if the boolean test is true
+    /// - Returns: the instance after called the `then`
     @discardableResult func declaredIf(_ value: Bool, _ then: (_ instance: Self) -> Void) -> Self {
         return declaredIf(value, then, otherwise: nil)
     }
 
+    /// Provides a then to configure instance inline based on a boolean test
+    /// - Parameter then: A then `self` as the argument if the boolean test is true
+    /// - Parameter otherwise: A then `self` as the argument if the boolean test is false
+    /// - Returns: the instance after called the `then` or `otherwise`
     @discardableResult func declaredIf(_ value: Bool, _ then: (_ instance: Self) -> Void, otherwise: ((_ instance: Self) -> Void)?) -> Self {
         if value {
             then(self)
@@ -51,28 +66,49 @@ public extension ObjectDHDeclarable {
 extension NSObject: ObjectDHDeclarable {}
 
 // MARK: - DHDeclarable
+/// Conform any value type to this protocol to instantiate inline
 public protocol DHDeclarable {
+    /// Generic for use with any type
     associatedtype T
 
     /// Provides a then to configure instances inline
     /// - Parameter then: A then with a mutable copy of `self` as the argument.
     /// - Returns: Simply returns the mutated copy of the instance after called the `then`.
     @discardableResult func declaredWith(_ then: (_ instance: inout T) -> Void) -> T
+
+    /// Provides a then to configure instance inline based on a boolean test
+    /// - Parameter then: A then `self` as the argument if the boolean test is true
+    /// - Returns: the instance after called the `then`
     @discardableResult func declaredIf(_ copy: inout T, _ value: Bool, _ then: (_ instance: inout T) -> Void) -> T
+
+    /// Provides a then to configure instance inline based on a boolean test
+    /// - Parameter then: A then `self` as the argument if the boolean test is true
+    /// - Parameter otherwise: A then `self` as the argument if the boolean test is false
+    /// - Returns: the instance after called the `then` or `otherwise`
     @discardableResult func declaredIf(_ copy: inout T, _ value: Bool, _ then: (_ instance: inout T) -> Void, otherwise: ((_ instance: inout T) -> Void)?) -> T
 }
 
 public extension DHDeclarable {
+    /// Provides a then to configure instances inline.
+    /// - Parameter then: A then `self` as the argument.
+    /// - Returns: Simply returns the instance after called the `then`.
     @discardableResult func declaredWith(_ then: (_ instance: inout Self) -> Void) -> Self {
         var copy = self
         then(&copy)
         return copy
     }
 
+    /// Provides a then to configure instance inline based on a boolean test
+    /// - Parameter then: A then `self` as the argument if the boolean test is true
+    /// - Returns: the instance after called the `then`
     @discardableResult func declaredIf(_ copy: inout T, _ value: Bool, _ then: (_ instance: inout T) -> Void) -> T {
         return declaredIf(&copy, value, then, otherwise: nil)
     }
 
+    /// Provides a then to configure instance inline based on a boolean test
+    /// - Parameter then: A then `self` as the argument if the boolean test is true
+    /// - Parameter otherwise: A then `self` as the argument if the boolean test is false
+    /// - Returns: the instance after called the `then` or `otherwise`
     @discardableResult func declaredIf(_ copy: inout T, _ value: Bool, _ then: (_ instance: inout T) -> Void, otherwise: ((_ instance: inout T) -> Void)?) -> T {
         if value {
             then(&copy)
--- Sources/DHDeclarable/Extensions/UIKit/UIViewController/UIViewController+as.swift
+++ Sources/DHDeclarable/Extensions/UIKit/UIViewController/UIViewController+as.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 public extension UIViewController {
+    /// Put this view controller in the `rootViewController` of a `UINavigationController` and return it
     var inNavigationController: UINavigationController {
         UINavigationController(rootViewController: self)
     }

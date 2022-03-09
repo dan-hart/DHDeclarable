29198972f0a3b6ef32fe852f44fc05b5e070b600
Dan Hart <dhart94@gmail.com>
2022-03-09 11:53:02 -0500

    Add pre-commit & pre-push tests/formatting

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/shibapm/Komondor.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -31,3 +31,13 @@ let package = Package(
         ),
     ]
 )
+
+#if canImport(PackageConfig)
+    import PackageConfig
+    let config = PackageConfiguration([
+        "komondor": [
+            "pre-commit": ["xcodebuild test -destination 'name=iPhone 13' -scheme 'DHDeclarable-Package'", "swift run swiftFormat ."],
+            "pre-push": "xcodebuild test -destination 'name=iPhone 13' -scheme 'DHDeclarable-Package'"
+        ],
+    ])
+#endif
